7f6fe97aacebd7f640d146e41a4da6ecf2ef447d
Nikola Todorovic <todorovicnik2@gmail.com>
2023-06-02 16:19:10 +0200

    feat: Update routes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -29,9 +29,9 @@ const Navbar = () => {
                 
                 
                 <ul className="links">
-                <li><a href="home">Home</a></li>
-                <li><a href="about">About</a></li>
-                <li><a href="services">Services</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/author">Authors</a></li>
+                <li><a href="/browse">Browse Songs</a></li>
                 <li><a href="contact">Contact</a></li>
                 </ul>
                 <a href="#" className="action-btn">Login</a>
