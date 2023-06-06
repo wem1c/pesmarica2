74f0775ca187eedf490a0115e701cd3a7868ec56
Conor C. Peterson <conorpetersondev@gmail.com>
2023-06-07 00:01:29 +0200

    feat: add additional fields to Song model
    Includes:
    - adding an Artist model
    - making migrations
    - registering Artist model in admin

--- backend/songs/admin.py
+++ backend/songs/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from songs.models import Song
+from songs.models import Song, Artist
+
 
 class SongAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-    search_fields = ('title',)
+    list_display = ('title', 'artist')
+    search_fields = ('title','artist__name')
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
 admin.site.register(Song, SongAdmin)
+admin.site.register(Artist, ArtistAdmin)
--- backend/songs/models.py
+++ backend/songs/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
-class Song(models.Model):
-    title = models.CharField(max_length=100)
-    # artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    # album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    # genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    lyrics = models.TextField()
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.title
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+class Song(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genre = models.CharField(max_length=100)
+    year = models.IntegerField()
+    lyrics = models.TextField()
+    #duration
