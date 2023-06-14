4a7f99784a2e8deca4c0d6186865115e4246bf01
Conor C. Peterson <conorpetersondev@gmail.com>
2023-06-14 18:32:27 +0200

    feat: add artist and owner fields to songs

--- backend/songs/models.py
+++ backend/songs/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Artist(models.Model):
     name = models.CharField(max_length=100)
+    #songs = models.ManyToManyField('Song', related_name='artists')
 
     def __str__(self):
         return self.name
@@ -14,8 +15,9 @@ class Song(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     album = models.CharField(max_length=100)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, related_name='songs', on_delete=models.CASCADE)
     genre = models.CharField(max_length=100)
     year = models.IntegerField()
     lyrics = models.TextField()
+    owner = models.ForeignKey('auth.User', related_name='songs', on_delete=models.CASCADE)
     #duration
