f835873a00a4607b59527fb4923f35c2bd510c07
Conor C. Peterson <conorpetersondev@gmail.com>
2023-06-14 18:36:12 +0200

    feat: update song and add artist views
    Includes:
    - adding custom permission to song view
    - creating new artist view

--- backend/songs/views.py
+++ backend/songs/views.py
@@ -1,9 +1,8 @@
 from rest_framework import viewsets
 from rest_framework import permissions
-
-from songs.models import Song
-
-from songs.serializers import SongSerializer
+from songs.models import Song, Artist
+from songs.serializers import SongSerializer, ArtistSerializer
+from songs.permissions import IsOwnerOrReadOnly
 
 
 class SongViewSet(viewsets.ModelViewSet):
@@ -12,5 +11,12 @@ class SongViewSet(viewsets.ModelViewSet):
     """
     queryset = Song.objects.all()
     serializer_class = SongSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+class ArtistViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows songs to be viewed or edited.
+    """
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
