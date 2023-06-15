124f6188f3e51b8a26470dbfee3e88f35edcffa1
Conor C. Peterson <conorpetersondev@gmail.com>
2023-06-15 16:59:30 +0200

    feat: add and config whitenoise
    whitenoise module provides automatic static file serving.
    
    Includes:
    - adding whitenoise middleware
    - adding whitenoise settings
    - adding whitenoise to the requirement list

--- backend/pesmarica2/settings.py
+++ backend/pesmarica2/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
+
+# Simplified static file serving.
+# https://pypi.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
