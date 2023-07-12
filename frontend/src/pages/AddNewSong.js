557c05273fc81b15a7f9d448a63512f9bcbe25d8
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2023-07-12 06:18:51 +0200

    delete old frontend project

--- frontend/src/pages/AddNewSong.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddNewSong = () => {
-  return (
-    <div>AddNewSong</div>
-  )
-}
-
-export default AddNewSong
--- frontend/src/pages/SingUp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SingUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
-
-export default SingUp
