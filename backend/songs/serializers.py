52c836a7e5c26b81a2823cf60fc06c9673e62dec
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2023-07-12 06:13:17 +0200

    modified SongSerializer to display artist name

--- backend/songs/serializers.py
+++ backend/songs/serializers.py
@@ -5,10 +5,19 @@ from songs.models import Song, Artist
 
 class SongSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    artist = serializers.SerializerMethodField()
 
     class Meta:
         model = Song
-        fields = ['url', 'id', 'created_at' ,'title', 'album', 'artist', 'genre', 'year', 'lyrics', 'owner']
+        fields = ['url', 'id', 'created_at', 'title', 'album',
+                  'artist', 'genre', 'year', 'lyrics', 'owner']
+
+    def get_artist(self, obj):
+        return {
+            'id': obj.artist.id,
+            'name': obj.artist.name
+        }
+
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
 
