a4e6349370011d8e0ed6361cddf93f221d7d477e
sulejmanhoxha <sulejman.hoxha2001@hotmail.com>
2023-08-09 03:32:29 +0200

    add react query to project

--- frontend-pesmarica2/src/main.jsx
+++ frontend-pesmarica2/src/main.jsx
@@ -1,13 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./App.jsx";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+				<ReactQueryDevtools initialIsOpen />
+			</BrowserRouter>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
